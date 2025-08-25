636bad97e892de3d303c779494f52c35a989c387
JavierTorresO <javitorres2022@udec.cl>
2025-08-25 16:45:04 -0400

    RED: test para contar las pintas sin tener en cuenta los as/comodines - falla como esperado

---
+++ tests/test_contador_pintas.py
@@ -0,0 +1,6 @@
+from src.contador_pintas import ContadorPintas
+
+def test_contar_pintas_sin_ases():
+    dados = [[2, 3], [4, 2]]  # total de dos "2"
+    contador = ContadorPintas()
+    assert contador.contar(dados, 2) == 2
