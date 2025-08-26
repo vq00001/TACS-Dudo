705231ae042e31ad0c1a06bd598106ac4262be67
Siiphs <jomatamala2023@udec.cl>
2025-08-25 20:43:42 -0400

    RED: test para multiples dados basado en clases - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -10,3 +10,20 @@ def test_tirar_dado(mocker):
    resultado = dado.tirar()
 
    assert resultado == 2
+
+def test_multiples_dados(mocker):
+   obj_dado1 = dado.dado()
+   obj_dado2 = dado.dado()
+
+   mock_random1 = mocker.patch('dado.random.randint')
+   mock_random1.return_value = 1
+
+   resultado1 = obj_dado1.tirar()
+   
+   mock_random2 = mocker.patch('dado.random.randint')
+   mock_random2.return_value = 3
+
+   resultado2 = obj_dado2.tirar()
+   
+   assert resultado1 == 1
+   assert resultado2 == 3
