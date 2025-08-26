1a48b3e9f29601be639b0c26f5d622b40df9e24b
JavierTorresO <javitorres2022@udec.cl>
2025-08-26 01:07:00 -0400

    GREEN: implementación mínima para contar pintas considerando Ases como comodines y caso especial de ronda de un dado(as no es comodin)

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -3,6 +3,21 @@ class ContadorPintas:
         """
         cuenta cuántas veces está el "numero"(pinta) indicada en la lista de los dados.
         !!no considera ases(1) como comodines para esta version de los test -> 1 es un numero normal por ahora, aun no es un comodin
-        
+
         """
         return dados.count(numero)
+    
+    def contar_pintas(self, dados, apuesta, ases_comodines=True):
+        """
+        Cuenta cuantas veces aparece la pinta apostada en la lista de dados.
+        - Los Ases (1) cuentan como comodines, excepto cuando la apuesta es por 1.
+        - Si ases_comodines=False, los Ases no cuentan como comodines. -> Se refiere a la ronda especial cuando un jugador queda por primera vez con un dado
+        """
+        contador = 0
+        for d in dados:
+            if d == apuesta:
+                contador += 1
+            elif d == 1 and apuesta != 1 and ases_comodines:
+                contador += 1
+        return contador
+
