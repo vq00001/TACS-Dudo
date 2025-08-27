53a09f1eaff713b703439cff548c79a547282f76
Siiphs <siiphs@outlook.com>
2025-08-27 16:58:17 -0400

    REFACTOR: corregir algunos nombres de variables y metodos

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -4,33 +4,33 @@ sys.path.append('src/juego')
 import dado
 
 def test_tirar_dado(mocker):
-   obj_dado = dado.dado()
+   dado = dado.dado()
    mock_random = mocker.patch('dado.random.randint')
    mock_random.return_value = 2
 
-   resultado = obj_dado.tirar()
+   resultado = dado.tirar()
 
    assert resultado == 2
 
-def test_multiples_dados(mocker):
-   obj_dado1 = dado.dado()
-   obj_dado2 = dado.dado()
+def test_tirar_multiples_dados(mocker):
+   dado1 = dado.dado()
+   dado2 = dado.dado()
 
    mock_random1 = mocker.patch('dado.random.randint')
    mock_random1.return_value = 1
 
-   resultado1 = obj_dado1.tirar()
+   resultado1 = dado1.tirar()
    
    mock_random2 = mocker.patch('dado.random.randint')
    mock_random2.return_value = 3
 
-   resultado2 = obj_dado2.tirar()
+   resultado2 = dado2.tirar()
    
    assert resultado1 == 1
    assert resultado2 == 3
 
-def test_get():
-   obj_dado = dado.dado()
-   resultado = obj_dado.tirar()
+def test_ver_dado():
+   dado = dado.dado()
+   resultado = dado.tirar()
 
-   assert obj_dado.get() == resultado
+   assert dado.ver() == resultado
