00e7051ca3241941252b4c1802f892ea492a632d
JavierTorresO <javitorres2022@udec.cl>
2025-08-27 15:06:44 -0400

    GREEN: agregan nuevos test de casos invalidos, separando segun sea el caso

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -1,22 +1,41 @@
 import pytest
 from juego.validador_apuesta import ValidadorApuesta
 
+#-------------------------------------------------------------------------------------------
+#casos comunes
+
 def test_set_get_apuesta_valida():
     val = ValidadorApuesta()
     val.set_apuesta(3, 5)
     cantidad, numero = val.get_apuesta()
     assert cantidad == 3
     assert numero == 5
-
+#-------------------------------------------------------------------------------------------
+#casos invalidos/erroneos
 def test_set_apuesta_invalida_cantidad():
     val = ValidadorApuesta()
     with pytest.raises(ValueError):
         val.set_apuesta(-2, 4)  # cantidad negativa no válida
 
-def test_set_apuesta_invalida_numero():
+def test_set_apuesta_invalida_cantidad_cero():
     val = ValidadorApuesta()
     with pytest.raises(ValueError):
-        val.set_apuesta(2, 7)  # número fuera de rango
+        val.set_apuesta(0, 3)  # cantidad 0 no es válida
+
+def test_set_apuesta_invalida_numero_mayor_a_seis():
+    val = ValidadorApuesta()
+    with pytest.raises(ValueError):
+        val.set_apuesta(2, 7)  # numero referente a pinta fuera de rango 
+
+def test_set_apuesta_invalida_numero_cero():
+    val = ValidadorApuesta()
+    with pytest.raises(ValueError):
+        val.set_apuesta(3, 0)  # pinta 0 no existe
+
+def test_set_apuesta_invalida_numero_negativo():
+    val = ValidadorApuesta()
+    with pytest.raises(ValueError):
+        val.set_apuesta(2, -1)
 
 def test_get_apuesta_sin_definir():
     val = ValidadorApuesta()
