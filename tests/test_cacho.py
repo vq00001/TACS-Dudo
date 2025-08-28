b2c93c04006d7b57ba84c256f4737de60dc48611
Siiphs <jomatamala2023@udec.cl>
2025-08-28 17:05:56 -0400

    RED: test para tirar todos los dados del cacho - falla como esperado

--- tests/test_cacho.py
+++ tests/test_cacho.py
@@ -6,3 +6,11 @@ import cacho
 def test_creacion_cacho():
     obj_cacho = cacho.cacho()
     assert len(obj_cacho.dados) == 5
+
+def test_tirar_dados():
+    obj_cacho = cacho.cacho()
+    dados = obj_cacho.tirar_dados()
+
+    for dado in dados:
+        valor = dado.ver()
+        assert valor >=1 and valor <= 6 and valor is not None
